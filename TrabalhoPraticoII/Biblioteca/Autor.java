ba603720a6a70f9495d74756c8e81b5f4ba75403
itpzzi <itpzzi@gmail.com>
2017-02-12 02:44:31 -0200

    The Odin Project

--- TrabalhoPraticoII/Biblioteca/Autor.java
+++
@@ -1,8 +0,0 @@
-package TrabalhoPraticoII.Biblioteca;
-
-public class Autor extends Pessoa {
-    
-    public Autor(int id, String nome, String sobrenome, int idade) {
-        super(id, nome, sobrenome, idade);
-    }
-}
